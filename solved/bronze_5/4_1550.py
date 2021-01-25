f94ea25906fb224c907c6de81797f693fa92e304
gon2gon2 <ark5354@gmail.com>
2021-01-25 23:05:14 +0900

    01.25

---
+++ solved/bronze_5/4_1550.py
@@ -0,0 +1,13 @@
+n_16 = input("16진수 입력")
+dic_16 = {'A': '10', 'B':'11', 'C':'12','D':'13','E':'14','F':'15'}
+n_16 = list(n_16)
+n_10 = []
+for n in n_16:
+    if n in dic_16:
+        n_10.append(dic_16[n])
+    else:
+        n_10.append(n)
+N = 0
+for idx in range(len(n_10)):
+    N += int(n_10[idx]) * 16 **idx
+print(N)
